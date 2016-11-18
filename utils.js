29b1904e9d9f250c63ce1caccd9f38ecd9cdb93c
mlison <michal.lison@nitor.fi>
2016-11-18 11:12:22 +0200

    added debounce

--- utils.js
+++ utils.js
@@ -6,3 +6,19 @@ utils.getParameterByName = function (name) {
     results = regex.exec(location.search);
 	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 };
+
+// David Walsh's debounce
+utils.debounce = function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
